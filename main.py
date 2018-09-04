da5830575c90e515879acfa22e792ead0af77c4c
bitw219 <42571246+bitw219@users.noreply.github.com>
2018-09-04 19:03:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'SHUBHAM IS HERE'
+  msg_body = 'SHUBHAM IS HERE'
+  SUBJECT = 'NOTIFY'
+  msg = MIMEMultipart()
+  msg.attach(MIMEText(msg_body,'plain'))
+  message = 'Subject: {}\n\n{}'.format(SUBJECT, msg_body)
+  server = smtplib.SMTP('smtp.gmail.com',25)
+  server.ehlo()
+  server.starttls()
+  server.ehlo()
+  server.login("aatish.jain@ellicium.com","9921610239")
+  server.sendmail("aatish.jain@ellicium.com",['aatish.jain@ellicium.com','shubham.shirude@ellicium.com'],message)
+  server.quit()
+  return msg_body
 
 if __name__ == '__main__':
   app.run()
