04db34456ceb444b573e9543ac73c1d2b595ed26
bitw219 <42571246+bitw219@users.noreply.github.com>
2018-09-05 11:25:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def hello_world():
   server.starttls()
   server.ehlo()
   server.login("aatish.jain@ellicium.com","9921610239")
-  server.sendmail("aatish.jain@ellicium.com",['aatish.jain@ellicium.com','shubham.shirude@ellicium.com'],message)
+  server.sendmail("aatish.jain@ellicium.com",['aatish.jain@ellicium.com','shubham.shirude@ellicium.com','sonali.dorle@ellicium.com'],message)
   server.quit()
   return msg_body
 
